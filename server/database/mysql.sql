3d1b92f2345d7322c50041a875d12ff68ec59624
MallquiDeone <159455849+MallquiDeone@users.noreply.github.com>
2024-02-08 11:49:20 -0500

    mysql db added

---
+++ server/database/mysql.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE db_asistencia;
+
+/*****  TABLA USUARIO *****/
+/*************************/
+CREATE TABLE tblUsuario(
+	usuario_id INT AUTO_INCREMENT,
+	tUsuario TEXT,
+    tContrasena TEXT,
+    rol_id INT,
+    tNombre VARCHAR(30),
+    tApellido VARCHAR(30),
+    PRIMARY KEY(usuario_id),
+    FOREIGN KEY(rol_id) REFERENCES tblRoles(rol_id)
+);
+
+/*****  TABLA ESTUDIANTE *****/
+/*************************/
+CREATE TABLE tblEstudiante(
+	estudiante_id INT AUTO_INCREMENT,
+    cDni CHAR(8) UNIQUE NOT NULL,
+    vNombres VARCHAR(50) NOT NULL,
+    vApellido_p VARCHAR(50) NOT NULL,
+    vApellido_m VARCHAR(50) NOT NULL,
+    url TEXT,
+    public_id TEXT,
+    estado INT,
+    PRIMARY KEY(estudiante_id)
+);
