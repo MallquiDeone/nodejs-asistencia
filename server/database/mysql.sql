fbd850778de007effcf8528d3d33b257c1abafbe
MallquiDeone <159455849+MallquiDeone@users.noreply.github.com>
2024-02-17 11:45:10 -0500

    tablas de db completo

--- server/database/mysql.sql
+++ server/database/mysql.sql
@@ -4,6 +4,19 @@ CREATE TABLE turno(
     hora_limite_entrada TIME,
     PRIMARY KEY(id_turno)
 );
+INSERT INTO turno (tipo_turno, hora_limite_entrada) VALUES ('Temprano', '08:00:00');
+CREATE TABLE grado(
+	id_grado INT AUTO_INCREMENT,
+    nombre_grado VARCHAR(50),
+    PRIMARY KEY(id_grado)
+);
+INSERT INTO grado (nombre_grado) VALUES ("1");
+CREATE TABLE seccion(
+	id_seccion INT AUTO_INCREMENT,
+    nombre_seccion VARCHAR(50),
+    PRIMARY KEY(id_seccion)
+);
+INSERT INTO seccion (nombre_seccion) VALUES("A");
 CREATE TABLE estudiantes(
     dni INT,
     nombres VARCHAR(50) NOT NULL,
@@ -13,9 +26,14 @@ CREATE TABLE estudiantes(
     url TEXT,
     public_id TEXT,
     id_turno INT NOT NULL,
+    id_grado INT NOT NULL,
+    id_seccion INT NOT NULL,
     PRIMARY KEY(dni),
-    FOREIGN KEY (id_turno) REFERENCES turno(id_turno)
+    FOREIGN KEY (id_turno) REFERENCES turno(id_turno),
+    FOREIGN KEY (id_grado) REFERENCES grado(id_grado),
+    FOREIGN KEY (id_seccion) REFERENCES seccion(id_seccion)
 );
+
 CREATE TABLE asistencia_general (
     id_asistencia INT AUTO_INCREMENT,
     dni INT NOT NULL,
